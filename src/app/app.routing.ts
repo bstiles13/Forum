954c929e790a82e7161180de932025a769b46ddd
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-08-18 14:48:53 -0600

    added thread component to router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,12 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { HistoryComponent } from './history/history.component';
 import { NewComponent } from './new/new.component';
+import { ThreadComponent } from './thread/thread.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HistoryComponent },
-    { path: 'new', component: NewComponent }
+    { path: 'new', component: NewComponent },
+    { path: 'thread/:number?', component: ThreadComponent}
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
