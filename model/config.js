9fa0941a3ed61526fcfefc8ae8c80403267ed913
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-11-10 16:11:11 -0700

    testing mysql pool

--- model/config.js
+++ model/config.js
@@ -14,31 +14,14 @@ var db_config = {
   database: "forum"
 };
 
-function handleDisconnect() {
-  if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-  } else {
-    connection = mysql.createConnection(db_config);
-  }
-
-  connection.connect(err => {
-    if (err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000);
-    }
-    console.log("Successful mysql connection");
-  });
-
-  connection.on('error', err => {
-    console.log('db error', err);
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
+if (process.env.JAWSDB_URL) {
+  connection  = mysql.createPool(process.env.JAWSDB_URL);  
+} else {
+  connection  = mysql.createPool(db_config);
 }
 
-handleDisconnect();
+connection.getConnection(function(err, connection) {
+  // connected! (unless `err` is set)
+});
 
 module.exports = connection;
