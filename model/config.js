289762040dc027011b1585e1cba70e91117ad770
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-11-10 14:50:24 -0700

    testing db disconnect handler

--- model/config.js
+++ model/config.js
@@ -2,25 +2,43 @@ const mysql = require('mysql');
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
+var db_config = {
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "1234",
+  database: "forum"
+};
+
+function handleDisconnect() {
+  if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
   } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-      
-        // Your username
-        user: "root",
-      
-        // Your password
-        password: "1234",
-        database: "forum"
-      });
+    connection = mysql.createConnection(db_config);
   }
-    
-    connection.connect(function(err) {
-      if (err) throw err;
-      console.log("Successful mysql connection at id: " + connection.threadId);
-    });
 
-    module.exports = connection;
+  connection.connect(err => {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+    console.log("Successful mysql connection");
+  });
+
+  connection.on('error', err => {
+    console.log('db error', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
